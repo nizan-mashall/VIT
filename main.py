d7066384892a83acae044e1751ee044ec03a340a
nizan-mashall <nizanm111@gmail.com>
2026-04-15 17:08:47 +0300

    test3

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from PIL import Image
 from model import processor
-image_processor = processor()
 
+img = Image.open('demo_image.png')
+
+image_processor = processor(img)
 image_processor._image_scaling()
--- model/image_processor.py
+++ model/image_processor.py
@@ -1,11 +1,14 @@
+from PIL import Image
+
 class processor:
-    def __init__(self, image = None):
-        self.image = image
+    def __init__(self, img):
+        self.img = img
    
     def _image_scaling(self):
+        self.img.show()
         print('Hey')
         return
     
-    def _split_to_patches(self,image = None):
+    def _split_to_patches(self, img):
         return
     
