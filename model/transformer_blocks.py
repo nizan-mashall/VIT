bd31f26b360d75fff2481053cfc4ddd90c740759
nizan-mashall <nizanm111@gmail.com>
2026-04-28 15:08:14 +0300

    complete the transformer blocks

--- model/transformer_blocks.py
+++ model/transformer_blocks.py
@@ -1,7 +1,43 @@
 import torch
 import torch.nn as nn
 
-class TransformerBlocks(nn.modules):
-    def __init__(self):
+class Attention(nn.Module):
+    def __init__(self, dim, n_heads, dropout_rate):
         super.__init__()
-        
+        self.n_heads = n_heads
+        self.attention = nn.MultiheadAttention(embed_dim=dim, num_heads=self.n_heads, dropout=dropout_rate, batch_first = True)
+
+    def forward(self, x):
+        attention_output, attention_weights = self.attention(x, x, x)
+        return attention_output
+    
+class FeedForward(nn.Sequential):
+    def __init__(self, dim, hidden_dim, dropout_rate = 0):
+        super.__init__(
+        nn.Linear(dim, hidden_dim),
+        nn.GELU(),
+        nn.Dropout(dropout_rate),
+        nn.Linear(hidden_dim, dim),
+        nn.Dropout(dropout_rate)
+        )
+
+class ResidualAdd():
+    def __init__(self, fn):
+        super.__init__()
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        res = x
+        x = self.fn(x, **kwargs)
+        x+=res
+        return x
+    
+class PreNorm():
+    def __init__(self, dim, fn):
+        super.__init__()
+        self.dim = dim
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
