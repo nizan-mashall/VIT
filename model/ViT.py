ea81bc9f85bf74490fd13c02e2a1aba9b133c8f7
nizan-mashall <nizanm111@gmail.com>
2026-05-06 08:58:50 +0300

    start writing VIT class

--- model/ViT.py
+++ model/ViT.py
@@ -5,10 +5,15 @@ from model import ImageProcessor
 from model import PatchEmbedding
 
 class VisionTransformer(nn.Module):
-    def __init__(self):
+    def __init__(self, img, h, w, patch_size, batch_size, depth, dim , n_heads, dropout_rate, hidden_dim):
         super().__init__()
-        self.image_processor
-        self.patchembedding
-        self.transformerencoder
+        self.image_processor = ImageProcessor(img, h, w, patch_size)
+        self.patch_embedding = PatchEmbedding(img, h, w, patch_size, batch_size)
+        self.transformer_encoder = TransformerEncoder(depth, dim , n_heads, dropout_rate, hidden_dim)
 
-        
+    def forward(self, x):
+        x = self.image_processor()
+        x = self.patch_embedding(x)
+pixel2embedding = PatchEmbedding(img_tensor, IMG_HEIGHT, IMG_WIDTH, PATCH_SIZE, BATCH_SIZE)
+img_embedding = pixel2embedding(img_tensor)
+
